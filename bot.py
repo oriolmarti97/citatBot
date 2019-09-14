774970510b3dd31e6d44bc16488553c914342960
Oriol Martí i Rodríguez <oriolmarti97@gmail.com>
2019-09-14 23:48:45 +0200

    si el tweet conté una imatge, hauria de fer la captura més gran per encabir-la

--- bot.py
+++ bot.py
@@ -12,9 +12,13 @@ api=tweepy.API(auth)
 
 def baixaIResponTweet(tweetABaixar, tweetARespondre):
     #Fem la captura
+    if not hasattr(tweetABaixar,'media') or tweetABaixar.media is None:
+        alcada=600
+    else:
+        alcada=1200
     ruta='./temp/'+tweetABaixar.id_str+'.png'
     url="https://twitter.com/%s/status/%s"%(tweetABaixar.user.screen_name,tweetABaixar.id_str)
-    os.system('xvfb-run cutycapt --url=%s --min-width=600 --out=%s 2>&1 >/dev/null'%(url,ruta))
+    os.system('xvfb-run cutycapt --url=%s --min-width=600 --min-height=%i --out=%s 2>&1 >/dev/null'%(url,alcada,ruta))
     
     try:
         api.update_with_media(ruta,status='@%s'%tweetARespondre.user.screen_name,in_reply_to_status_id=tweetARespondre.id_str)
@@ -32,8 +36,8 @@ class ElMeuEscoltador(tweepy.StreamListener):
             api.update(status='@%s :('%status.user.screen_name,in_reply_to_status_id=status.id_str)
             return
         status_replied=api.get_status(status.in_reply_to_status_id)
-        if status_replied.quoted_status_id is None:
-            if status_replied.in_reply_to_status_id is None:
+        if not hasattr(status_replied,'quoted_status_id') or status_replied.quoted_status_id is None:
+            if not hasattr(status_replied,'in_reply_to_status_id') or status_replied.in_reply_to_status_id is None:
                 #El tweet al que respon no cita ni respon a ningú. Com que hi ha respost, el veu, de manera que no cal captura
                 api.update(status='@%s :('%status.user.screen_name,in_reply_to_status_id=status.id_str)
                 return
