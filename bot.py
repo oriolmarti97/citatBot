45438cd583cef46fd3f213b8db633edcb7006c2f
Oriol <oriolmarti97@gmail.com>
2019-09-28 23:57:18 +0200

    Hauria de respondre amb fotos de gatets en cas de no funcionar

--- bot.py
+++ bot.py
@@ -70,13 +70,13 @@ class ElMeuEscoltador(tweepy.StreamListener):
                 return
         if status.in_reply_to_status_id is None:
             #Ni tan sols respon a ningú
-            api.update(status='@%s Hola :D'%status.user.screen_name,in_reply_to_status_id=status.id_str)
+            api.update_with_media('gat saludant.jpg',status='@%s Hola :D'%status.user.screen_name,in_reply_to_status_id=status.id_str)
             return
         status_replied=api.get_status(status.in_reply_to_status_id)
         if not hasattr(status_replied,'quoted_status_id') or status_replied.quoted_status_id is None:
             if not hasattr(status_replied,'in_reply_to_status_id') or status_replied.in_reply_to_status_id is None:
                 #El tweet al que respon no cita ni respon a ningú. Com que hi ha respost, el veu, de manera que no cal captura
-                api.update(status='@%s :('%status.user.screen_name,in_reply_to_status_id=status.id_str)
+                api.update_with_media('gat trist',status='@%s :('%status.user.screen_name,in_reply_to_status_id=status.id_str)
                 return
             citat=api.get_status(status_replied.in_reply_to_status_id)
         else:
